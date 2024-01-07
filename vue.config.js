a3439a64144b05a2c991b320764e776daccae289
maoyanluo <maoyanluo@pinduoduo.com>
2024-01-07 11:34:42 +0800

    build at docs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+// const path = require('path')
 module.exports = {
   lintOnSave: false,
+  outputDir: './docs'
+
+  // build: {
+  //   // Template for index.html
+  //   index: path.resolve(__dirname, './docs/index.html'),
+
+  //   // Paths
+  //   assetsRoot: path.resolve(__dirname, './docs'),
+  //   assetsSubDirectory: 'static',
+  //   assetsPublicPath: './',
+  // }
 }
